275afcfd69394ae75454b8ac2d4a330918bc3d1e
Thomas Jansson <tjansson60@gmail.com>
2018-04-13 12:30:53 +0000

    Add script to install python libs

---
+++ python-install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Download the lastest 3.6 python
+
+# Install 
+conda install shapely pyproj tqdm seaborn xlrd
+conda install -c conda-forge folium
+pip install pyvisgraph pyshp shapely geojson geopy openpyxl flask bpython
