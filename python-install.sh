1428d70dbe5a64bc2e9a6cb1066d7754c74fc5b1
Thomas Jansson <tjansson60@gmail.com>
2018-04-18 09:16:04 +0200

    Add keras to install

--- python-install.sh
+++ python-install.sh
@@ -3,6 +3,6 @@
 # Download the lastest 3.6 python
 
 # Install 
-conda install shapely pyproj tqdm seaborn xlrd
+conda install shapely pyproj tqdm seaborn xlrd keras
 conda install -c conda-forge folium
 pip install pyvisgraph pyshp shapely geojson geopy openpyxl flask bpython fuzzywuzzy
