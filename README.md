b8d294d3d85babb9d764589139e1ad593ee8f15a
Thomas Jansson <tjansson60@gmail.com>
2018-04-29 11:01:59 +0200

    Removed fish references, updated ubuntu script

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
 # About
 
 This might be a interesting template for others, but mostly it is just for me to checkout on new machines.  
-
-# Fish shell
-
-* `apt install fonts-powerline`
-* `sudo add-apt-repository ppa:fish-shell/release-2` Get the lastest fish on ubuntu 16.04
-* Install fish on the shell and setup **fisher**
-* `curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher`
-* `fisher edc/bass omf/theme-bobthefish`
-* `chsh -s /usr/bin/fish `
-* `~/.config/fish/config.fish`
--- ubuntu-workstation-fresh-install.sh
+++ ubuntu-workstation-fresh-install.sh
@@ -5,7 +5,8 @@
 # * Setup anaconda enviroment
 # * Setup printers 
 # * Setup rsnapshot (bohr)
-# * Setup autofs for NFS mount (bohr)
+# * Setup autofs to enable auto mount NFS on /net (bohr). Modify: /etc/auto.master
+# * Setup fixed IPs in /etc/hosts with local network names
 
 # Find out what programs that have manually been installed on a system (usufull when clean-installing at upgrade):
 # comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u)
@@ -14,7 +15,7 @@
 sudo apt install autofs fonts-powerline fonts-roboto vim meld keepass2 filezilla gimp gimp-ufraw evince baobab htop\
     acidrip vlc calibre redshift darktable guake gkrellm nmap rsnapshot pdftk recordmydesktop x2goclient handbrake\
     iptraf sshfs bmon nethogs google-chrome-stable gparted build-essential fdupes flake8 git mediainfo pwgen\
-    texlive-latex-extra texlive-latex-recommended ubuntu-restricted-extras 
+    texlive-latex-extra texlive-latex-recommended texlive-lang-european ubuntu-restricted-extras nfs-common docker  
 
 # VS code and SQL operations
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
