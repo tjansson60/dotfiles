6b455112c22c37b8bfe33ed8bd75d7a6619609cd
Thomas Jansson <tjansson60@gmail.com>
2018-04-29 12:37:59 +0200

    Use snap to install vscode and slack

--- ubuntu-workstation-fresh-install.sh
+++ ubuntu-workstation-fresh-install.sh
@@ -13,12 +13,13 @@
 
 # Standard tools needed for install 
 sudo apt install autofs fonts-powerline fonts-roboto vim meld keepass2 filezilla gimp gimp-ufraw evince baobab htop\
-    acidrip vlc calibre redshift darktable guake gkrellm nmap rsnapshot pdftk recordmydesktop x2goclient handbrake\
-    iptraf sshfs bmon nethogs google-chrome-stable gparted build-essential fdupes flake8 git mediainfo pwgen\
+    acidrip vlc calibre redshift darktable guake gkrellm nmap rsnapshot recordmydesktop x2goclient handbrake\
+    iptraf sshfs bmon nethogs gparted build-essential fdupes flake8 git mediainfo pwgen\
     texlive-latex-extra texlive-latex-recommended texlive-lang-european ubuntu-restricted-extras nfs-common docker  
 
+# Download the google chrome deb and install 
+
 # VS code and SQL operations
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt install sqlops code 
+sudo snap install --classic vscode
+sudo snap install --classic slack
+# Download SQL operations from MS site
