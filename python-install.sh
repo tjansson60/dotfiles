0a3f9b6a6e2793b541ab2b6fb496a65f00757ce1
Thomas Jansson <tjansson60@gmail.com>
2018-04-29 14:05:58 +0200

    Removed thano and cleaned up a bit

--- python-install.sh
+++ python-install.sh
@@ -3,7 +3,10 @@
 # Download the lastest python 3 through Anaconda:
 # https://www.anaconda.com/download/#linux
 
+# conda update -n base conda
+# pip install --upgrade pip 
+
 # Install 
-conda install shapely pyproj tqdm seaborn xlrd keras theano
-conda install -c conda-forge tensorflow folium
+conda install shapely pyproj tqdm seaborn xlrd
+conda install -c conda-forge keras tensorflow folium
 pip install pyvisgraph pyshp shapely geojson geopy openpyxl flask bpython fuzzywuzzy
