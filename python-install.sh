e63ec8e658fb64c2a02bb612873c83a2a82364f1
Thomas Jansson <tjansson60@gmail.com>
2018-04-29 13:48:41 +0200

    Add tensorflow, theano to python-install.sh

--- python-install.sh
+++ python-install.sh
@@ -4,6 +4,6 @@
 # https://www.anaconda.com/download/#linux
 
 # Install 
-conda install shapely pyproj tqdm seaborn xlrd keras
-conda install -c conda-forge folium
+conda install shapely pyproj tqdm seaborn xlrd keras theano
+conda install -c conda-forge tensorflow folium
 pip install pyvisgraph pyshp shapely geojson geopy openpyxl flask bpython fuzzywuzzy
