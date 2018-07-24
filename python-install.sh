61040f9a6de5e64f63819daf04cd0a1b130ca5aa
Thomas Jansson <tjansson60@gmail.com>
2018-07-24 09:18:48 +0200

    Add google-cloud-storage to install script, update pip

--- python-install.sh
+++ python-install.sh
@@ -9,4 +9,6 @@
 # Install 
 conda install -y shapely pyproj tqdm seaborn xlrd
 conda install -y -c conda-forge keras tensorflow folium
+pip install --upgrade pip
 pip install msgpack pymysql pyvisgraph pyshp shapely geojson geopy openpyxl flask bpython fuzzywuzzy faker bcrypt feather-format
+pip install google-cloud-storage
