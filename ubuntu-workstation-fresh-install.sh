9cf2b81f7462154f5f5a1890f8261afd0c7ebfb4
Thomas Jansson <tjansson60@gmail.com>
2018-08-16 23:26:34 +0200

    Cleanup

--- ubuntu-workstation-fresh-install.sh
+++ ubuntu-workstation-fresh-install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Installation on workstation: 
-# * unetbootin 
+# Installation on workstation:
+# * unetbootin
 # * Setup anaconda enviroment
-# * Setup printers 
+# * Setup printers
 # * Setup rsnapshot (bohr)
 # * Setup autofs to enable auto mount NFS on /net (bohr). Modify: /etc/auto.master
 # * Setup fixed IPs in /etc/hosts with local network names
@@ -11,19 +11,19 @@
 # Find out what programs that have manually been installed on a system (usufull when clean-installing at upgrade):
 # comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u)
 
-# Standard tools needed for install 
+# Standard tools needed for install
 sudo apt install autofs fonts-powerline fonts-roboto vim meld keepass2 filezilla gimp gimp-ufraw evince baobab htop\
-    acidrip vlc calibre redshift darktable guake gkrellm nmap rsnapshot recordmydesktop x2goclient handbrake\
+    acidrip vlc calibre redshift-gtk darktable guake gkrellm nmap rsnapshot recordmydesktop x2goclient handbrake\
     iptraf sshfs bmon nethogs gparted build-essential fdupes flake8 git mediainfo pwgen\
     texlive-latex-extra texlive-latex-recommended texlive-lang-european ubuntu-restricted-extras nfs-common docker.io\
     pylint curl eog fonts-hack fonts-hack-ttf
 
-# Download the google chrome deb and install 
+# Download the google chrome deb and install
+# Download DBeaver for SLQ work, or SQL operations for MSSQL
 
 # VS code and SQL operations
 sudo snap install --classic vscode
 sudo snap install --classic slack
-# Download SQL operations from MS site
 
 # Setup dockers
 sudo gpasswd -a $USER docker
