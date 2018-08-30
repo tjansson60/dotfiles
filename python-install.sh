a6f35d1b7acbc38363b0b41ae3b4dd3b815226ed
Thomas Jansson <tjansson60@gmail.com>
2018-08-30 22:39:27 +0200

    Add mysql-connector to pip install

--- python-install.sh
+++ python-install.sh
@@ -10,5 +10,7 @@
 conda install -y shapely pyproj tqdm seaborn xlrd
 conda install -y -c conda-forge keras tensorflow folium
 pip install --upgrade pip
-pip install msgpack pymysql pyvisgraph pyshp shapely geojson geopy openpyxl flask bpython fuzzywuzzy faker bcrypt feather-format
+pip install msgpack pymysql pyvisgraph pyshp shapely geojson geopy \
+    openpyxl flask bpython fuzzywuzzy faker bcrypt feather-format \
+    mysql-connector
 pip install --upgrade google-cloud-storage google-cloud-bigquery
