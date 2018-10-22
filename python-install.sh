69f5fdd9e65133da6d0b40f47caa68874cefb828
Thomas Jansson <tjansson60@gmail.com>
2018-10-22 22:52:58 +0200

    Updated dependencies

--- python-install.sh
+++ python-install.sh
@@ -3,16 +3,22 @@
 # Download the lastest python 3 through Anaconda:
 # https://www.anaconda.com/download/#linux
 
+# Update pip and conda
+# conda update -n base -c defaults conda
 # conda update -n base conda
 # pip install --upgrade pip 
 
 # Install 
 conda install -y shapely pyproj tqdm seaborn xlrd
 conda install -y -c conda-forge keras tensorflow folium
+
 pip install --upgrade pip
+
 pip install msgpack pymysql pyvisgraph pyshp shapely geojson geopy \
     openpyxl flask bpython fuzzywuzzy faker bcrypt feather-format \
-    mysql-connector geojsoncontour py-spy afinn nltk black shap plotly pandas-gbq
+    mysql-connector geojsoncontour py-spy afinn nltk black shap \
+    plotly pandas-gbq lime pdfminer.six
+
 pip install --upgrade google-cloud-storage google-cloud-bigquery 
 
 # For NLTK run this to download sources:
