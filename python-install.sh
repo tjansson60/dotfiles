a897f1c24383f5310ce21db936025180fb55f6d8
Thomas Jansson <tjansson60@gmail.com>
2019-02-26 23:28:15 +0100

    Added descartes and mplleaflet

--- python-install.sh
+++ python-install.sh
@@ -40,7 +40,7 @@ pip install msgpack pymysql pyvisgraph pyshp shapely geojson geopy \
     openpyxl flask bpython fuzzywuzzy faker bcrypt feather-format \
     mysql-connector geojsoncontour py-spy afinn nltk black shap \
     plotly pandas-gbq lime pdfminer.six geojsoncontour xgboost \
-    selenium dexplot geocoder fpdf geopandas
+    selenium dexplot geocoder fpdf geopandas descartes mplleaflet
 
 pip install --upgrade google-cloud-storage google-cloud-bigquery 
 
